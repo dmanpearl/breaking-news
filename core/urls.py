32fe1575769aff6feb994154316f904735371cd4
David Manpearl <dmanpearl@pixelmonks.com>
2026-02-28 08:19:15 -0800

    fix(railway): update redirectview for root

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from . import views
 
 app_name = "core"
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/messages/"), name="home"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("profile/", views.profile_view, name="profile"),
